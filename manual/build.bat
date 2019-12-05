6ed3d64f43a8714a0ed89e1a45001eeb06fced05
kudzurunner <sdio@tuta.io>
2019-12-06 00:35:26 +0300

    updated VS to 2019

--- manual/build.bat
+++ manual/build.bat
@@ -1,14 +1,14 @@
 @echo on
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 set MINICONDA="C:\Miniconda3-x64"
 set PATH=%MINICONDA%;%MINICONDA%/Scripts/;%MINICONDA%/Library/bin;%PATH%
 set LIB=%MINICONDA%/Library/lib;%LIB%
 set CPATH=%MINICONDA%/Library/include;%CPATH%
 set CC=clang-cl
 set CXX=clang-cl
-set CFLAGS=-m64 -fmsc-version=1916
-set CXXFLAGS=-m64 -fmsc-version=1916
+set CFLAGS=-m64 -fmsc-version=1923
+set CXXFLAGS=-m64 -fmsc-version=1923
 set CONAN_CMAKE_GENERATOR=Ninja
 conan create . kudzurunner/stable -s compiler=clang -s compiler.libcxx=libstdc++ -s compiler.version=5.0 -s build_type=Debug
 conan create . kudzurunner/stable -s compiler=clang -s compiler.libcxx=libstdc++ -s compiler.version=5.0 -s build_type=Release
